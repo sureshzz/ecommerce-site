01f05dce7f692df82cb424799be3c2e8042ecd58
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-09 18:08:13 +0545

    display prodt in store page

--- ecommercesite/ecommercesite/urls.py
+++ ecommercesite/ecommercesite/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from . import views
-from django.urls import path
+from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('store/',include('store.urls')),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- ecommercesite/store/views.py
+++ ecommercesite/store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def store(request):
+  products = Product.objects.all().filter(is_available = True)
+  product_count = products.count()
+  context = {
+    "products" : products,
+    "productcount" : product_count
+  }
+  return render(request,'store/store.html',context)
