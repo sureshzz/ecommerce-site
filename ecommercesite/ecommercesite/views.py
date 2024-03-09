8feb27ee54f64fa522dcd71624a3c63ddd41015f
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-09 15:55:21 +0545

    display homepage

--- ecommercesite/ecommercesite/views.py
+++ ecommercesite/ecommercesite/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 def home(request):
-  return render(request,'home.html')
+  Products = Product.objects.all().filter(is_available= True)
+  print(Products)
+  context = {
+    'products' :  Products,
+  }
+  return render(request,'home.html',context)
   # return HttpResponse("hello")
