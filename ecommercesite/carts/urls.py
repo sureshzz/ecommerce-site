c0f3c0af568950770e37343ebc053dd565aa5c68
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-15 21:41:14 +0545

    remove_cart

--- ecommercesite/carts/urls.py
+++ ecommercesite/carts/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
   path('',views.cart,name = 'cart'),
   path('addcart/<int:product_id>/',views.add_cart,name = 'add_cart'),
+  path('removecart/<int:product_id>/',views.remove_cart,name = 'remove_cart'),
 ]
--- ecommercesite/carts/views.py
+++ ecommercesite/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from store.models import Product
 from .models import Cart , CartItem
@@ -43,6 +43,8 @@ def cart(request,total=0, quantity = 0 ,cart_items = None):
     for cart_item in cart_items:
       total += ( cart_item.quantity * cart_item.product.price)
       quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
   except ObjectDoesNotExist:
     pass
   
@@ -50,6 +52,19 @@ def cart(request,total=0, quantity = 0 ,cart_items = None):
     'total' : total,
     'quantity': quantity,
     'cart_items': cart_items,
+    'tax': tax,
+    'grand_total' : grand_total,
     
   }
   return render(request,'store/cart.html',context)
+
+def remove_cart(request,product_id):
+  cart = Cart.objects.get(cart_id = _cart_id(request))
+  product = get_object_or_404(Product,id = product_id)
+  cart_item = CartItem.objects.get(product= product,cart = cart)
+  if cart_item.quantity > 1:
+    cart_item.quantity -= 1
+    cart_item.save()
+  else:
+    cart_item.delete()
+  return redirect('cart')
