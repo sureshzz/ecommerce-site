e736c9086e35c064969d721ced06f3b9a4fe4fff
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-19 20:51:35 +0545

    check if prdt at cart

--- ecommercesite/store/views.py
+++ ecommercesite/store/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render ,get_object_or_404,HttpResponse
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
 
 # Create your views here.
 def store(request,category_slug = None):
@@ -32,12 +34,13 @@ def product_detail(request,category_slug,product_slug):
   print(product_slug)
   try:
     single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
-  
+    in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request),product=single_product).exists
   except Exception as e:
     raise e
   
   context = {
     'single_product' : single_product,
+    'in_cart' : in_cart,
     }
   
   return render(request,'store/product_detail.html',context)
