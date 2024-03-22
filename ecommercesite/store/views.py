7e58d38032e08d15c4f4421936b597ea6e1d6dd1
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-22 21:47:18 +0545

    search functionality

--- ecommercesite/store/views.py
+++ ecommercesite/store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import PageNotAnInteger,Paginator,EmptyPage
+from django.db.models import Q
 
 # Create your views here.
 def store(request,category_slug = None):
@@ -22,7 +23,7 @@ def store(request,category_slug = None):
     product_count = products.count()
   
   else:
-    products = Product.objects.all().filter(is_available = True)
+    products = Product.objects.all().filter(is_available = True).order_by('id')
     paginator = Paginator(products,2)
     page = request.GET.get('page')
     paged_products = paginator.get_page(page)
@@ -31,7 +32,7 @@ def store(request,category_slug = None):
   
   context = {
     "products" : paged_products,
-    "productcount" : product_count
+    "product_count" : product_count
   }
   return render(request,'store/store.html',context)
 
@@ -51,3 +52,20 @@ def product_detail(request,category_slug,product_slug):
     }
   
   return render(request,'store/product_detail.html',context)
+
+
+
+def search(request):
+  if 'keyword' in request.GET:
+    keyword = request.GET['keyword']
+    if keyword:
+      products = Product.objects.order_by('created_date').filter(Q(description__icontains = keyword) | Q(product_name__icontains = keyword))
+      product_count = products.count()
+    context = {
+      'products':products,
+      'product_count' : product_count,
+    }
+    
+  
+  
+  return render(request,'store/store.html',context)
