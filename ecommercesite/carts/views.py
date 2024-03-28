0e8fb1c35b30c1e5ee6b50233aa5b361af06812f
Suresh Thapamagar <sureshthapamagar8914@gmail.com>
2024-03-28 20:53:41 +0545

    updated variation models

--- ecommercesite/carts/views.py
+++ ecommercesite/carts/views.py
@@ -11,22 +11,20 @@ def _cart_id(request):
   return cart
 
 def add_cart(request,product_id):
-  product = Product.objects.get(id=product_id)
-  product_variation = []
+ 
+  # product_variation = []
   if request.method == "POST":
     for item in request.POST:
       key = item
-      value = request.POST[key]
-      
+      value = request.POST[key]   
       try:
-        variation = Variation.objects.get(product = product , variation_cagtegory__iexact = key, variation_value__iexact = value )
-        product_variation.append(variation)
+        variation = Variation.objects.get(variation_cagtegory__iexact = key, variation_value__iexact = value )
+        # product_variation.append(variation) product = product ,
+        print(variation)
       except:
         pass  
-      
-      
-      
-  product = Product.objects.get(id = product_id) #getprdt
+  product = Product.objects.get(id=product_id)
+            
   try:
     cart = Cart.objects.get(cart_id = _cart_id(request))
   except Cart.DoesNotExist:
@@ -38,6 +36,9 @@ def add_cart(request,product_id):
   
   try:
     cart_item = CartItem.objects.get(product = product,cart = cart)
+    # if len(product_variation) > 0:
+    #   for item in product_variation:
+    #     cart_item.variation.add(item)
     cart_item.quantity += 1
     cart_item.save()
   except CartItem.DoesNotExist :
@@ -46,6 +47,9 @@ def add_cart(request,product_id):
       quantity = 1,
       cart = cart,
     )
+    # if len(product_variation) > 0:
+    #   for item in product_variation:
+    #     cart_item.variation.add(item) 
     
     cart_item.save()
   return redirect('cart')
